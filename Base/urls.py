199c85d997d39bcfcab461c0bd557f13b5a85c2e
chrismushaz <Mushabati6jr@gmail.com>
2025-06-20 12:45:26 +0200

    Updated contact success alert and logic

--- Base/urls.py
+++ Base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import contact
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('success/', TemplateView.as_view(template_name='success.html'), name='success'),
+    
     
 ]
--- Base/views.py
+++ Base/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, 'home.html')
 
 
-def contact(request):
+"""def contact(request):
     if request.method == "POST":
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -28,4 +28,36 @@ def contact(request):
 
         return JsonResponse({'status': 'success', 'message': 'Thank you for contacting me! Your message has been saved.'})
 
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method. Only POST is allowed.'}, status=400)
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method. Only POST is allowed.'}, status=400)"""
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # Validation
+        if len(name) <= 1 or len(name) >= 30:
+            return render(request, 'contact.html', {
+                'error': 'Name should be greater than 2 and less than 30 characters.'
+            })
+
+        if len(email) <= 1 or len(email) >= 30:
+            return render(request, 'contact.html', {
+                'error': 'Invalid email, try again.'
+            })
+
+        if len(phone) <= 9 or len(phone) >= 13:
+            return render(request, 'contact.html', {
+                'error': 'Invalid phone number. It should be 10 to 12 digits.'
+            })
+
+        # Save the data to the database
+        ins = models.Contact(name=name, email=email, message=message, phone=phone)
+        ins.save()
+
+        # Render success page
+        return render(request, 'success.html')
+
+    return render(request, 'contact.html')  # fallback GET
